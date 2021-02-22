0c3d80bf20ec19d831f2b76baafa16d1fe52e3a7
zjwei <wzjg13002@163.com>
2021-02-22 19:09:22 +0800

    four

--- src/test/java/com/qizi/day4/execl4j/User.java
+++ src/test/java/com/qizi/day4/execl4j/User.java
@@ -3,12 +3,30 @@ package com.qizi.day4.execl4j;
 import com.github.crab2died.annotation.ExcelField;
 import lombok.*;
 
-@Data
+import java.util.Collections;
+
+
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
 @EqualsAndHashCode
-public class User {
+public class User{
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+    }
+
     @ExcelField(title = "用户名")
     private String username;
     @ExcelField(title = "密码")
